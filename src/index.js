65d78b97e1c83838f9ea14fbc9beff579b0720dd
Vincde <145802178+Vincde@users.noreply.github.com>
2024-05-22 19:29:19 +0200

    First commit adds input element and gets API for the entered city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+(function getCity() {
+  const bttn = document.querySelector("button");
+  const inputCity = document.getElementById("city");
+
+  bttn.addEventListener("click", () => {
+    getAPI(inputCity.value);
+  });
+})();
+
+async function getAPI(name) {
+  const weather = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=ee0f8ecf676b472ebc1102115242005&q=${name}`,
+    { mode: "cors" }
+  );
+
+  const data = await weather.json();
+
+  console.log(data.location);
+}
