dec6d054e12d15020aae02fe014324b4a1f85f05
Vincde <145802178+Vincde@users.noreply.github.com>
2024-05-23 19:56:50 +0200

    Add clearing of page, new elements in the webpage to print info, pattern for input

--- src/index.js
+++ src/index.js
@@ -5,27 +5,68 @@ import "./style.css";
   const inputCity = document.getElementById("city");
 
   bttn.addEventListener("click", () => {
+    if (inputCity.validity.patternMismatch) {
+      console.log("invalid type of city in input field");
+      return;
+    }
+    clearPara();
     getAPI(inputCity.value);
   });
 })();
 
+(function printDOMWebPage() {
+  let i = 1;
+
+  const body = document.querySelector("body");
+  const containerOfResults = document.createElement("div");
+
+  while (i <= 9) {
+    const paragraph = document.createElement("p");
+    containerOfResults.appendChild(paragraph);
+    i++;
+  }
+
+  body.appendChild(containerOfResults);
+})();
+
 async function getAPI(name) {
-  const weather = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=ee0f8ecf676b472ebc1102115242005&q=${name}`,
-    { mode: "cors" }
-  );
+  try {
+    const weather = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=ee0f8ecf676b472ebc1102115242005&q=${name}`,
+      { mode: "cors" }
+    );
 
-  const data = await weather.json();
+    const data = await weather.json();
 
-  workDataAndPrintIt(data);
+    workDataAndPrintIt(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function workDataAndPrintIt(data) {
   let rest;
+  let weather = data.current.condition.text;
 
   ({ ...rest } = data.location);
+  printDataOnScreen(weather, rest);
+}
+
+function printDataOnScreen(weather, rest) {
+  let counter = 1;
+  const paragraphs = document.querySelectorAll("div > p");
+  paragraphs[0].textContent = `Weather: ${weather}`;
 
   for (let key in rest) {
-    console.log(`${key} : ${rest[key]}`);
+    paragraphs[counter].textContent = `${key} : ${rest[key]}`;
+    counter++;
+  }
+}
+
+function clearPara() {
+  const paras = document.querySelectorAll("body > p");
+
+  for (let elem of paras) {
+    elem.textContent = "";
   }
 }
